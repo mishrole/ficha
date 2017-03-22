72dcb504c587df5e33b49965afad14711e47734a
Mitchell Rodríguez <mitrodle@gmail.com>
2017-03-22 12:57:45 -0500

    limpiando comentarios

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,9 @@
-/*
-
-var ficha = document.getElementById("fichas");
-
-var nombre    = prompt("Ingrese su nombre");
-var apellido  = prompt("Ingrese su apellido");
-var dni       = prompt("Ingrese su número de dni");
-var direccion = prompt("Ingrese su dirección");
-
-function imprimirFicha(nombre, apellido, dni, direccion){
-  
-  return "Nombre: " + nombre + "<br>" +
-         "Apellido: " + apellido + "<br>" +
-         "DNI: " + dni + "<br>" +
-         "Dirección: " + direccion + "<br>"
-};
-
-//ficha.innerHTML = imprimirFicha(nombre, apellido, dni, direccion);
-
-document.write(imprimirFicha(nombre, apellido, dni, direccion));
-
-*/
-
 function imprimirFicha(nombre, apellido, dni, direccion){
 
 	var nombre = document.getElementById("nombre");
 	var apellido = document.getElementById("apellido");
 	var dni = document.getElementById("dni");
 	var direccion = document.getElementById("direccion");
-//	var enviar = document.getElementById("enviar");
 	var record = document.getElementById("records");
 
 	if((nombre.value.length == 0) || (apellido.value.length == 0) || (dni.value.length == 0) || (direccion.value.length == 0)){
@@ -44,8 +20,3 @@ function imprimirFicha(nombre, apellido, dni, direccion){
 	}
 
 };
-
-/* enviar.onclick = function(){
-	records.innerHTML = imprimirFicha(nombre,apellido,dni,direccion)
-}
-*/
